69842c4b45fa24279e3b6bab711e5af2d7537bf7
Utkarsh23112002 <utkarshpattanshetti45@gmail.com>
2024-11-29 20:03:00 +0530

    sample4

--- src/Demo/Sample4.java
+++ src/Demo/Sample4.java
@@ -4,7 +4,7 @@ public class Sample4
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Running Sample 3 Class From Demo");
+		System.out.println("Running Sample 4 Class From Demo");
 		
 	}
 
