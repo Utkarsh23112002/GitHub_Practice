8775af8734ad8cb1a8043ab4094cff15c83c07e3
Utkarsh23112002 <utkarshpattanshetti45@gmail.com>
2024-11-29 19:43:03 +0530

    Sample2

--- src/Demo/Sample1.java
+++ src/Demo/Sample1.java
@@ -4,7 +4,7 @@ public class Sample1
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Runnin Sample 1 Class");
+		System.out.println("Runnin Sample 1 Class From Demo");
 		
 	}
 
---
+++ src/Demo/Sample2.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Sample2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Runnin Sample 2 Class From Demo");
+		
+	}
+
+}
